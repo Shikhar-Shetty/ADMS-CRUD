5a372fa6fee3cb6b3a1bd11fb950c6084610d451
AneeshMBhat <aneeshbhat3719@gmail.com>
2023-01-10 23:57:56 +0530

    done

--- frontend/src/components/AddRainfall.js
+++ frontend/src/components/AddRainfall.js
@@ -1,11 +1,9 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
 
-export default function AddCustomer(props) {
+export default function AddRainfall(props) {
   const [name, setName] = useState("");
-  const [addr, setAddr] = useState("");
-  const [mob, setMob] = useState("");
-  const [ema, setEma] = useState("");
+  const [density, setDensity] = useState("");
 
   useEffect(() => {
     loadUpdate();
@@ -14,27 +12,21 @@ export default function AddCustomer(props) {
   const loadUpdate = () => {
     if (props.update != {}) {
       setName(props.update.name);
-      setMob(props.update.mobileNo);
-      setAddr(props.update.address);
-      setEma(props.update.emailAddress);
+      setDensity(props.update.density);
     }
   };
 
   const submitHandler = async (e) => {
     e.preventDefault();
     if (props.update.id == null) {
-      await axios.post("http://localhost:5000/customer", {
+      await axios.post("http://localhost:5000/rainfall", {
         name: name,
-        address: addr,
-        mobileNo: mob,
-        emailAddress: ema,
+        density: density,
       });
     } else {
-      await axios.patch(`http://localhost:5000/customer/${props.update.id}`, {
+      await axios.patch(`http://localhost:5000/rainfall/${props.update.id}`, {
         name: name,
-        address: addr,
-        mobileNo: mob,
-        emailAddress: ema,
+        density: density,
       });
     }
     props.setUpdate({});
@@ -45,44 +37,28 @@ export default function AddCustomer(props) {
 
   const resetHandler = () => {
     setName("");
-    setAddr("");
-    setMob("");
-    setEma("");
+    setDensity("");
   };
 
   return (
     <form onSubmit={(e) => submitHandler(e)} className="w-1/2 flex">
       <div className="bg-blue-900 flex flex-col my-auto w-full rounded p-2 text-white">
         <div className="text-3xl font-bold underline text-center rounded p-1 ">
-          Customer Form
+          Rainfall Form
         </div>
-        <label className="p-1">Enter Customer Name</label>
+        <label className="p-1">Enter Rainfall Name</label>
         <input
           className="rounded-lg text-black p-1"
           type="text"
           onChange={(e) => setName(e.target.value)}
           value={name}
         />
-        <label className="p-1">Enter Address</label>
+        <label className="p-1">Enter Rainfall Density</label>
         <input
           className="rounded-lg text-black p-1"
           type="text"
-          onChange={(e) => setAddr(e.target.value)}
-          value={addr}
-        />
-        <label className="p-1">Enter Mobile Number</label>
-        <input
-          className="rounded-lg text-black p-1"
-          type="text"
-          onChange={(e) => setMob(e.target.value)}
-          value={mob}
-        />
-        <label className="p-1">Enter Email Address</label>
-        <input
-          className="rounded-lg text-black p-1"
-          type="text"
-          onChange={(e) => setEma(e.target.value)}
-          value={ema}
+          onChange={(e) => setDensity(e.target.value)}
+          value={density}
         />
         <div className="flex flex-row p-1 mt-2 justify-center">
           <button type="submit" className="bg-green-900 w-1/3 p-1 rounded mr-1">
